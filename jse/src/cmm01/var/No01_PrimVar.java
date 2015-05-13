0095fb67b25d43db131ae028d413ec27bce15cf7
iamjhe <cjfchal@naver.com>
2015-05-13 09:56:32 +0900

    ifelse

---
+++ jse/src/cmm01/var/No01_PrimVar.java
@@ -0,0 +1,18 @@
+package cmm01.var;
+
+public class No01_PrimVar {
+	public static void main(String[] args) {
+		/*
+		 * ������ ���� 8����
+		 * ���� Ÿ�� : char
+		 * ���� Ÿ��(���� Ÿ��) : byte, short, int, long
+		 * ���� Ÿ��(�Ǽ� Ÿ��) : float, double
+		 * �? Ÿ�� : boolean
+		 * */
+		int x; //������ ����
+		
+		x = 1; // �Ҵ翬���ڸ� ����� ���� �Ҵ�(����)
+		
+		int y;
+	}
+}
