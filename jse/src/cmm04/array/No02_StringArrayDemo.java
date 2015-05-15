39c1930f3691aca2ff9c733ab871b3df34baf242
iamjhe <cjfchal@naver.com>
2015-05-15 11:19:14 +0900

    https://github.com/iamjhe/jse.git

--- jse/src/cmm04/array/No02_StringArrayDemo.java
+++ jse/src/cmm04/array/No02_StringArrayDemo.java
@@ -2,7 +2,16 @@ package cmm04.array;
 
 public class No02_StringArrayDemo {
 	public static void main(String[] args) {
-		String[] juso = {"서울,종로,광화문"};
-		
+		String[] juso1 = {"서울","종로","광화문"};
+		/* 
+		String[] juso = new String[3]; 
+		juso[0] = "서울"; 
+		juso[1] = "종로"; 
+		juso[2] = "광화문"; 
+		 */ 
+		for( int i = 0; i < juso1.length; i ++ ){ 
+			System.out.println(" 주소 : " + juso1[i]); 
+
+		}
 	}
 }
---
+++ jse/src/cmm04/array/No06_GugudanDemo.java
@@ -0,0 +1,14 @@
+package cmm04.array;
+
+public class No06_GugudanDemo {
+	public static void main(String[] args) {
+		for (int dan = 2; dan < 10; dan++) {
+			for (int num = 1; num < 10; num++) {
+				System.out.println(dan + "*" + num + "=" + (dan * num));
+			}
+			System.out.println();
+			System.out.println("\n");
+		}
+
+	}
+}
